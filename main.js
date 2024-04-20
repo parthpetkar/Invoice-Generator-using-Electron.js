5883d21ca98ef22657ecb6be5a078d22b627dbca
parthpetkar <callofdutyparth2004@gmail.com>
2024-04-20 14:09:31 +0530

    Invoice DB Added

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const mysql = require('mysql2/promise');
-const { prependListener } = require('process');
-const fs = require('fs');
-const ExcelJS = require('exceljs');
-require('dotenv').config();
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const mysql = require("mysql2/promise");
+const { prependListener } = require("process");
+const fs = require("fs");
+const ExcelJS = require("exceljs");
+require("dotenv").config();
 
 let connection;
 
@@ -14,74 +14,83 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true,
-            preload: path.join(__dirname, 'Backend/preload.js')
-        }
+            preload: path.join(__dirname, "Backend/preload.js"),
+        },
     });
 
-    win.loadFile('public/index.html');
+    win.loadFile("public/index.html");
     win.maximize();
 }
 
 app.whenReady().then(() => {
     createWindow();
 
-    app.on('activate', () => {
+    app.on("activate", () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
         }
     });
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-async function connectToDB() { // Corrected function name and async keyword
+async function connectToDB() {
+    // Corrected function name and async keyword
     try {
-        connection = await mysql.createConnection({ // Assign connection to the global variable
+        connection = await mysql.createConnection({
+        // Assign connection to the global variable
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
+            database: process.env.DB_DATABASE,
         });
-    } catch (error) { // Added error parameter
-        console.error('Error connecting to database:', error); // Corrected log message and added error parameter
+    } catch (error) {
+        // Added error parameter
+        console.error("Error connecting to database:", error); // Corrected log message and added error parameter
     }
 }
 
 // Call connectToDB function when app is ready
 app.whenReady().then(connectToDB);
 
-ipcMain.on('insertMilestone', async (event, data) => {    
+ipcMain.on("insertMilestone", async (event, data) => {
     const { rowDataArray, projectData } = data;
     var c_name = projectData.customerName;
 
     try {
-        const [result] = await connection.execute(`SELECT cin FROM customers where company_name = '${c_name}'`);
+        const [result] = await connection.execute(
+            `SELECT cin FROM customers where company_name = '${c_name}'`
+        );
         const cin = result[0].cin;
 
-        rowDataArray.forEach(rowData => {
+        rowDataArray.forEach((rowData) => {
             const { milestone, claimPercentage, amount } = rowData;
             const query = `INSERT INTO milestones (cin, pono, milestone_name, claim_percent, amount) VALUES (?, ?, ?, ?, ?)`;
-            connection.query(query, [cin, projectData.poNo, milestone, claimPercentage, amount], (error, results, fields) => {
-                if (error) throw error;
-            });
+            connection.query(
+                query,
+                [cin, projectData.poNo, milestone, claimPercentage, amount],
+                (error, results, fields) => {
+                    if (error) throw error;
+                }
+            );
         });
-
     } catch (error) {
-        console.error('Error inserting project data:', error);
+        console.error("Error inserting project data:", error);
     }
-})
+});
 
-ipcMain.on('createProject', async (event, data) => {
+ipcMain.on("createProject", async (event, data) => {
     const { projectData } = data;
     var c_name = projectData.customerName;
-    
-    try {
 
-        const [result] = await connection.execute(`SELECT cin FROM customers where company_name = '${c_name}'`);
+    try {
+        const [result] = await connection.execute(
+            `SELECT cin FROM customers where company_name = '${c_name}'`
+        );
         const cin = result[0].cin;
 
         const insertProjectQuery = `
@@ -96,14 +105,13 @@ ipcMain.on('createProject', async (event, data) => {
             projectData.projectName,
         ]);
 
-        console.log('Data inserted successfully');
-        
+        console.log("Data inserted successfully");
     } catch (error) {
-        console.error('Error inserting project data:', error);
+        console.error("Error inserting project data:", error);
     }
-})
+});
 
-ipcMain.on('createCustomer', async (event, data) => {
+ipcMain.on("createCustomer", async (event, data) => {
     const { customerData } = data;
     try {
         const insertCustomerQuery = `
@@ -111,30 +119,76 @@ ipcMain.on('createCustomer', async (event, data) => {
           VALUES (?, ?, ?, ?, ?, ?)
         `;
         await connection.query(insertCustomerQuery, [
-          customerData.companyName,
-          customerData.address,
-          customerData.phone,
-          customerData.gstin,
-          customerData.pan,
-          customerData.cin,
+            customerData.companyName,
+            customerData.address,
+            customerData.phone,
+            customerData.gstin,
+            customerData.pan,
+            customerData.cin,
         ]);
-    
-        console.log('Data inserted successfully');
-      } catch (error) {
-        console.error('Error inserting data:', error);
+
+        console.log("Data inserted successfully");
+    } catch (error) {
+        console.error("Error inserting data:", error);
     }
 });
 
-ipcMain.on('createInvoice', async (event, data) => {
-    //console.log(invoicedata);
-    const invoiceData = data.invoiceData;
-    const { formData, milestones } = invoiceData;
-    //console.log(formData, milestones);
-    //const getcinandponoquery = ``;
+ipcMain.on("createInvoice", async (event, data) => {
+    const invoiceData = data.invoiceData; // Accessing the 'invoiceData' property
 
+    // Extract formData and milestones from invoiceData
+    const { formData, milestones } = invoiceData;
+
+    // Inserting data into Invoices table
+    const formdatainsert = `
+        INSERT INTO Invoices (cin, pono, company_name, project_name, invoice_number, invoice_date, due_date, taxes_excluded, total_prices, milestone_name, remaining_amount)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    // Assuming taxes_excluded is same as total_prices
+    // Calculating remaining_amount based on already paid amounts for each milestone
+    await connection.query(formdatainsert, [
+        milestones[0].cin, // Assuming all milestones belong to the same project, thus the same 'cin' and 'pono'
+        milestones[0].pono,
+        formData.customer,
+        milestones[0].project_name, // Assuming all milestones belong to the same project, thus the same project name
+        formData.invoiceNumber,
+        formData.invoiceDate,
+        formData.dueDate,
+        milestones[0].total_prices, // Assuming taxes_excluded is same as total_prices
+        milestones[0].total_prices, // Initial value for remaining_amount
+        milestones.map((milestone) => milestone.milestone_name).join(", "), // Concatenating all milestone names
+        calculateRemainingAmount(milestones), // Calculating remaining_amount
+    ]);
+    function calculateRemainingAmount(milestones) {
+        const totalProjectPrice = parseFloat(milestones[0].total_prices);
+        const totalPaidAmount = milestones.reduce(
+            (total, milestone) => total + parseFloat(milestone.amount),
+            0
+        );
+        return totalProjectPrice - totalPaidAmount;
+    }
+
+    async function updateMilestoneStatus(milestones) {
+        const updateStatusQuery = `
+            UPDATE milestones
+            SET status = 'paid'
+            WHERE cin = ? AND pono = ? AND milestone_name IN (?)
+        `;
+
+        const milestoneNames = milestones.map(
+            (milestone) => milestone.milestone_name
+        );
+        await connection.query(updateStatusQuery, [
+            milestones[0].cin,
+            milestones[0].pono,
+            milestoneNames,
+        ]);
+    }
+    await updateMilestoneStatus(milestones);
 });
 
-ipcMain.on('createForm', async (event, data) =>{
+ipcMain.on("createForm", async (event, data) => {
     //console.log(data);
     //sending data to excel
     const invoiceData = data.invoiceData;
@@ -143,67 +197,79 @@ ipcMain.on('createForm', async (event, data) =>{
     console.log(milestones);
 
     const workbook = new ExcelJS.Workbook();
-    workbook.xlsx.readFile('IEC_Invoice_template.xlsx')
-     .then(() => {
-        const worksheet = workbook.getWorksheet('Invoice 2');
-        if (worksheet) {
+    workbook.xlsx
+        .readFile("IEC_Invoice_template.xlsx")
+        .then(() => {
+            const worksheet = workbook.getWorksheet("Invoice 2");
+            if (worksheet) {
             // Update cell values with invoice data
-            worksheet.getCell('A13').value = formData.customer;
-            worksheet.getCell('A14').value = formData.project;
-            worksheet.getCell('F4').value = formData.invoiceNumber;
-            worksheet.getCell('F3').value = formData.invoiceDate;
-            worksheet.getCell('F5').value = formData.dueDate;
-            worksheet.getCell('A21').value = formData.description;
-            return workbook.xlsx.writeFile('generatedInvoice.xlsx');
-        } else {
-        throw new Error('Worksheet not found in the Excel file.');
-        }
-    })
-    .then(() => {
-        console.log('Invoice generated successfully!');
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-})
+                worksheet.getCell("A13").value = formData.customer;
+                worksheet.getCell("A14").value = formData.project;
+                worksheet.getCell("F4").value = formData.invoiceNumber;
+                worksheet.getCell("F3").value = formData.invoiceDate;
+                worksheet.getCell("F5").value = formData.dueDate;
+                worksheet.getCell("A21").value = formData.description;
+                return workbook.xlsx.writeFile("generatedInvoice.xlsx");
+            } else {
+                throw new Error("Worksheet not found in the Excel file.");
+            }
+        })
+        .then(() => {
+            console.log("Invoice generated successfully!");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+});
 
-ipcMain.handle('fetchData', async (event) => {
+ipcMain.handle("fetchData", async (event) => {
     try {
-        const [customer_rows] = await connection.execute('SELECT * FROM customers');
-        const [milestone_rows] = await connection.execute('SELECT * FROM milestones');
+        const [customer_rows] = await connection.execute("SELECT * FROM customers");
+        const [milestone_rows] = await connection.execute(
+            "SELECT * FROM milestones"
+        );
         return { customers: customer_rows, milestones: milestone_rows };
     } catch (error) {
-        console.error('Error fetching data from database:', error);
+        console.error("Error fetching data from database:", error);
     }
 });
 
-ipcMain.handle('fetchCustomer', async (event) => {
+ipcMain.handle("fetchCustomer", async (event) => {
     try {
-        const [company_name ] = await connection.execute('SELECT company_name FROM customers');
+        const [company_name] = await connection.execute(
+            "SELECT company_name FROM customers"
+        );
         return { company_name };
     } catch (error) {
-        console.error('Error fetching data from database:', error);
+        console.error("Error fetching data from database:", error);
     }
 });
-ipcMain.handle('fetchProject', async (event, companyName) => {
+ipcMain.handle("fetchProject", async (event, companyName) => {
     try {
-        const [projects] = await connection.execute('SELECT projects.project_name FROM projects INNER JOIN customers ON projects.cin = customers.cin WHERE customers.company_name = ?', [companyName]);
+        const [projects] = await connection.execute(
+            "SELECT projects.project_name FROM projects INNER JOIN customers ON projects.cin = customers.cin WHERE customers.company_name = ?",
+            [companyName]
+        );
         return { projects };
     } catch (error) {
-        console.error('Error fetching data from database:', error);
+        console.error("Error fetching data from database:", error);
     }
 });
-ipcMain.handle('fetchMilestones', async (event, projectName) => {
+ipcMain.handle("fetchMilestones", async (event, projectName) => {
     try {
-        const [milestones] = await connection.execute('SELECT * FROM milestones INNER JOIN projects ON milestones.cin = projects.cin AND milestones.pono = projects.pono WHERE projects.project_name = ?', [projectName]);
+        const [milestones] = await connection.execute(
+            "SELECT * FROM milestones INNER JOIN projects ON milestones.cin = projects.cin AND milestones.pono = projects.pono WHERE projects.project_name = ?",
+            [projectName]
+        );
         return { milestones };
     } catch (error) {
-        console.error('Error fetching data from database:', error);
+        console.error("Error fetching data from database:", error);
     }
 });
 // Close database connection when app is quit
-app.on('quit', () => {
-    if (connection) { // Check if connection exists before trying to end it
+app.on("quit", () => {
+    if (connection) {
+    // Check if connection exists before trying to end it
         connection.end();
     }
 });
